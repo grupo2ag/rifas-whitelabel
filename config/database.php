6a7664549dfe65648c828833dfa730e37e09bfbf
figueiredo <rafael@artiz.com.br>
2024-06-10 11:23:04 -0300

    redis elasticache nao funciona

--- config/database.php
+++ config/database.php
@@ -146,28 +146,6 @@ return [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
-
-        //'clusters' => [
-            'default' => [
-                [
-                    'scheme' => env('REDIS_SCHEME', 'tcp'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => env('REDIS_DB', '0'),
-                ],
-            ],
-            'cache' => [
-                [
-                    'scheme' => env('REDIS_SCHEME', 'tcp'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => env('REDIS_CACHE_DB', '1'),
-                ],
-            ],
-        //],
-        /*
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -185,7 +163,6 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-        */
     ],
 
 ];
