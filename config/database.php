16f31bcb9f3052bdbc9547d0aab968e68610150c
figueiredo <rafael@artiz.com.br>
2024-06-10 10:25:04 -0300

    redis cache

--- config/database.php
+++ config/database.php
@@ -147,6 +147,27 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
+        'clusters' => [
+            'default' => [
+                [
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
+            'cache' => [
+                [
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
+        ],
+        /*
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -164,7 +185,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
+        */
     ],
 
 ];
